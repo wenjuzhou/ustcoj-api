cf5bf7c371bfd4e757ecbdcce57fbd9ba4534e02
Jiyan He <volltin@live.com>
2018-07-16 10:48:31 +0800

    del urls

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,11 +1,1 @@
 from django.conf.urls import include, url
-from rest_framework.routers import DefaultRouter
-
-from user import views
-
-router = DefaultRouter()
-router.register(r'', views.UserViewSet)
-
-urlpatterns = [
-    url(r'', include(router.urls))
-]
