d7f573c9a526d02bbbce9e7d9654b6586ce11369
Jiyan He <volltin@live.com>
2018-07-16 14:50:36 +0800

    add migrate scripts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 NAME=ustcoj_api
-DOCKER=ustcoj_api
-APP=$(cd `dirname $0`; pwd)/app
-docker stop $NAME && docker rm $NAME
+MIGRATE_NAME=ustcoj_api_migrate
+IMAGE_NAME=ustcoj_api
+APP_DIR=$(cd `dirname $0`; pwd)/app
+
+# migrate
+docker stop ${MIGRATE_NAME} && docker rm ${MIGRATE_NAME}
+docker run -d \
+  -e MODULE=${NAME} \
+  -v ${APP_DIR}:/code \
+  --name ${MIGRATE_NAME} \
+  ${IMAGE_NAME} \
+  python3 manage.py migrate
+
+# api server
+docker stop ${NAME} && docker rm ${NAME}
 docker run -d \
   -p 8000:8000 \
-  -e MODULE=$NAME \
-  -v $APP:/code \
-  --name $NAME \
-  $DOCKER
+  -e MODULE=${NAME} \
+  -v ${APP_DIR}:/code \
+  --name ${NAME} \
+  ${IMAGE_NAME}
